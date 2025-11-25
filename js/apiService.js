57239a7c0dd9643a3e318224cf0db7e19c0094d9
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-11-25 17:12:50 -0300

    link do back hospedado atualizado

--- js/apiService.js
+++ js/apiService.js
@@ -1,7 +1,7 @@
 // js/apiService.js
 // Serviço centralizado para comunicação com a API
 
-const API_BASE_URL = 'bbts-api-production.up.railway.app';
+const API_BASE_URL = 'https://bbts-api.onrender.com/';
 
 // Configuração padrão do fetch
 const defaultHeaders = {
